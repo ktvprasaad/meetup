13fb9cd6b973d6b42d77d8d89203edfc3f8a1613
ktvprasaad <47123069+ktvprasaad@users.noreply.github.com>
2020-02-17 09:53:58 -0500

    Updated for Alert
    Updated for Alert when keyed in city not found and new file Alert.js

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { getSuggestions } from './api';
+import { InfoAlert } from './Alert';
 
 class CitySearch extends Component {
 
@@ -11,10 +12,19 @@ class CitySearch extends Component {
     handleInputChanged = (event) => {
         const value = event.target.value;
         this.setState({ query: value });
-        getSuggestions(value).then(suggestions => 
-            this.setState({
-                suggestions
-        }));
+        getSuggestions(value).then(suggestions => {
+            this.setState({ suggestions });
+            
+            if (value && getSuggestions.length === 0 ) {
+                this.setState({
+                    infoText: "We cannot find the city you are looking for. Please try another city",
+                });
+            } else {
+                this.setState({
+                    infoText: '',
+                });
+            }
+        });
     }
 
     handleItemClicked = (value, lat, lon) => {
@@ -25,6 +35,7 @@ class CitySearch extends Component {
     render() {
         return (
             <div className="CitySearch">
+                <InfoAlert text={this.state.infoText} />
                 <input
                     type="text"
                     className="city"
