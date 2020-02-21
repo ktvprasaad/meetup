253209ec3ab20e649986535f63001803a8ccf292
ktvprasaad <47123069+ktvprasaad@users.noreply.github.com>
2020-02-20 20:13:59 -0500

    PWA Changes
    Files updated and included as part of PWA Changes.

--- src/index.js
+++ src/index.js
@@ -10,7 +10,8 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
+serviceWorker.register();
 atatus.config('d320769e06c14cba8fe7b79b7feb41bf').install();
 
 atatus.notify(new Error('Test Atatus Setup'));
